2e325451e9cd82ec28fe5f4752d387114638f7b3
jnkt15 <jnk.ttm@gmail.com>
2020-11-30 18:46:58 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,4 +10,10 @@ class Item < ApplicationRecord
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :shipping_date
 
+  with_options presence: true do
+    validates :name, :text, :price
+  end
+
+  validates :category_id, :condition_id, :deliveryfee_id, :prefecture_id, :shipping_date_id, numericality: { other_than: 1 }
+
 end
