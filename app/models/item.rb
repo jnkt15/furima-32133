be30bb93a1170c3cf8a0dc5e7b7ef7a07b0e7cda
jnkt15 <jnk.ttm@gmail.com>
2020-11-30 18:18:41 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,5 +3,11 @@ class Item < ApplicationRecord
   has_one :order
   has_one_attached :image
 
-  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :deliveryfee
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :shipping_date
+
 end
